f1f63ba39ef41f345ff029c1b551516bff45a6c7
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-08-24 06:42:39 +0100

    updated to include input function, feature-Name and linear estimate.

--- linear-regression/linear-regression.py
+++ linear-regression/linear-regression.py
@@ -17,5 +17,38 @@ y_eval = dfeval.pop('survived')
 # dftrain.age.hist(bins=20)
 # dftrain.sex.value_counts().plot(kind='barh')
 # dftrain['class'].value_counts().plot(kind='barh')
-pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survived')
-plt.show()
+# pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survived')
+# plt.show()
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if shuffle:
+            ds = ds.shuffle(1000)
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds
+    return input_function
+
+for feature_name in CATEGORICAL_COLUMNS:
+    vocabulary = dftrain[feature_name].unique() # gets a list of all unique values from given feature column
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+linear_est.train(train_input_fn)
+# result = linear_est.evaluate(eval_input_fn)
+result = list(linear_est.predict(eval_input_fn))
+
+print(dfeval.loc[4])
+print(y_eval.loc[4])
+print(result[4]['probabilities'][1])
